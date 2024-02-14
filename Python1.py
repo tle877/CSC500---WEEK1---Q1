9a43ecaa62fc495622b9b3add9dbe966075df702
tle877 <60452318+tle877@users.noreply.github.com>
2024-02-14 15:33:44 -0800

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -5,7 +5,6 @@ addition_result = num1 + num2
 subtraction_result1 = num1 - num2
 subtraction_result2 = num2 - num1
 
-# Print the results
 print("The addition of", num1, "and", num2, "is:", addition_result)
 print("The subtraction of", num1, "and", num2, "is:", subtraction_result1)
 print("The subtraction of", num2, "and", num1, "is:", subtraction_result2)
