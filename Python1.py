a171ec183da4fdee91d26313265ad1aa2a4a20c6
tle877 <60452318+tle877@users.noreply.github.com>
2024-02-14 15:06:36 -0800

    Adding/Subtraction  2 numbers

---
+++ Python1.py
@@ -0,0 +1,9 @@
+num1 = float(input("Please input the first number: "))
+num2 = float(input("Please input the second number: "))
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+
+# Print the results
+print("The addition of", num1, "and", num2, "is:", addition_result)
+print("The subtraction of", num1, "and", num2, "is:", subtraction_result)
