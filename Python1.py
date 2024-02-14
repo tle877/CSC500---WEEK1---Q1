c07491512e89a5db6fcba234b950dba9e50f7573
tle877 <60452318+tle877@users.noreply.github.com>
2024-02-14 15:39:50 -0800

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -1,10 +1,10 @@
 num1 = float(input("Please input the first number: "))
 num2 = float(input("Please input the second number: "))
 
-addition_result = num1 + num2
-subtraction_result1 = num1 - num2
-subtraction_result2 = num2 - num1
+addition= num1 + num2
+subtraction1 = num1 - num2
+subtraction2= num2 - num1
 
-print("The addition of", num1, "and", num2, "is:", addition_result)
-print("The subtraction of", num1, "and", num2, "is:", subtraction_result1)
-print("The subtraction of", num2, "and", num1, "is:", subtraction_result2)
+print("The addition of", num1, "and", num2, "is:", addition)
+print("The subtraction of", num1, "and", num2, "is:", subtraction1)
+print("The subtraction of", num2, "and", num1, "is:", subtraction2)
